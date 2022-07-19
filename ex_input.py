2bc7395ca89c2f848ae955ee3f1d0b3cbb48a445
atzmondahan <atzmon.dahan@outlook.com>
2022-07-19 16:58:38 +0300

    Add files via upload

---
+++ ex_input.py
@@ -0,0 +1,48 @@
+#count=int(input("Number of items: "))
+##ch=0
+##while ch != "q":
+##   # for i in range(count):
+##     ch=input("Enter a charcter: ")
+##     dechr=ord(ch)
+##     dec=dechr-0x30
+##     print('Hex value(ch)', "=",  hex(dechr),'decimal value =', dec , end="\n")
+##     print("{0} {1}".format(hex(dechr), (dec)))
+##     print((hex(dechr), (dec)))
+##print("done")
+#page 9 ex5
+# num=10.0
+# num=num+20
+# print(num)
+
+# num=num-25
+# print(num)
+
+# num=num*2
+# print(num)
+
+num=3
+num /=3
+print(num)
+
+# num=num**2
+# print(num)
+
+# #page 9 ex7
+
+# ##chr=input()
+# ##print(chr)
+# ##print(chr*2)
+# ##print(chr*3)
+
+# #page 10 ex10
+# num1=input("Enter number1: ")
+# print(num1)
+# num2=input("Enter number2: ")
+# print(f"{num1}+{num2}={int(num1)+int(num2)}")
+
+num = int(input("Enter number: "))
+print(num,num+1,num+2,num**2,end="\n")
+
+
+
+ 
---
+++ ex_print.py
@@ -0,0 +1,98 @@
+
+
+
+#x="Hello world"
+#print(x)
+#print(type (x))
+
+
+x=10
+print(x)
+print(type (x))
+
+
+
+
+
+x=6.2
+print(x)
+print(type (x))
+
+x=3 > 4
+print(x)
+print(type (x))
+
+a,b,c=2,1,4
+d="Hello"
+e="world"
+
+print("{0} {1} {2}".format(c,b,a))
+
+print(f"{a} {b} {c}")
+
+a,b,c=2,1,"hello"
+print(f"a={a} b={b} c={c}")
+
+print(d+" "+e+"!")
+
+##data = [ 
+##        ['year', 'last', 'first'], 
+##         [1943, 'Idle', 'Eric'], 
+##        [1939, 'Cleese', 'John'] 
+##       ]
+##for row in data:
+##    print(*row, sep=',')
+
+#import time
+
+
+
+##def count_items(items):
+##print('Counting ', end='', flush=True)
+##num = 0
+##for item in range (15):
+##    num += 1
+##    time.sleep(1)
+##    t=time.localtime()
+##    print(t.tm_hour,t.tm_min,t.tm_sec,sep=':')
+##    print('.', end='')
+##print(f'\nThere were {num} items')
+##
+"""
+This is a comment
+written in 
+more that just one line
+"""
+
+print('Hello, World!', end=' | ')
+print(100, end=' | ')
+print(5 + 5)
+
+
+print("hello","world", sep=' ')
+print(10,5,sep='|')
+print(f"{d}{e}",sep='|')
+
+print(20%5)
+print(21%5)
+print(22%5)
+print(23%5)
+print(24%5)
+print(25%5)
+print(26%5)
+print(27%5)
+print(28%5)
+print(29%5)
+print(30%5)
+print(31%5)
+print(32%5)
+print(33%5)
+print(34%5)
+print(30%5)
+
+
+
+
+
+
+
